004c3d9d39cdf4148bde5ff99de6b13ff197d5d2
anthony <anthony_cheng@comcast.com>
2019-05-15 15:41:10 -0400

    updated crawler to take user input and added a library folder to hold dependencies

--- README.md
+++ README.md
@@ -16,7 +16,6 @@ Here's a walkthrough of implemented user stories:
 
 ## Notes
 
-Describe any challenges encountered while building the app.
 
 ## License
 
--- src/CrawlerTest.java
+++ src/CrawlerTest.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class CrawlerTest {
 
     public static void main(String[] args) {
         Crawler webCrawler = new Crawler();
-        webCrawler.search("https://www.nba.com/", "lebron");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the url you want to search: ");
+        String url = scanner.nextLine();
+        System.out.println("Enter the keyword you want to search");
+        String keyword = scanner.nextLine();
+        webCrawler.search(url, keyword);
     }
 }
